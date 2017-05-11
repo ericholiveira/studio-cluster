9a8ef063b44db464b4db6ea68eb12a113c4c4ed1
Erich Oliveira <ericholiveira10@gmail.com>
2017-05-11 15:55:12 -0300

    fix wss usage

--- src/transport/primus/server.js
+++ src/transport/primus/server.js
@@ -7,7 +7,6 @@ var logging = require('./../../logging');
 
 var generateId = function(){
     'use strict';
-
     return uuid.v4();
 };
 
@@ -17,23 +16,23 @@ function PrimusTransport(instanceId,port,serverOpt,clientOpt,Studio){
 	var refs = {};
 	serverOpt = serverOpt || {};
 	serverOpt.port = port;
-    serverOpt.instanceId = instanceId;
-    serverOpt.https = function () { this.isSecure = true; };
-    serverOpt.http = function () { this.isSecure = false; };
+	serverOpt.instanceId = instanceId;
+	serverOpt.https = function () { this.isSecure = true; };
+	serverOpt.http = function () { this.isSecure = false; };
 	this._Studio = Studio;
-	this._clientOpt = clientOpt;
+	this._clientOpt = clientOpt || {};
 	this._connections = {};
 	this._server = Primus.createServer(function connection(spark) {
 		spark.on('data', function(message) {
-  			if(message && message.r){
-                refs[message.r] = refs[message.r] || Studio(message.r); 
-                refs[message.r].apply(null,message.p).then(function(res){
-                    spark.write({i:message.i, m:res,s:1});
-                }).catch(function(err) {
-                    err = serializeError(err);
-                    spark.write({i: message.i, m: err, s: 0});
-                });
-            }
+			if(message && message.r){
+				refs[message.r] = refs[message.r] || Studio(message.r);
+				refs[message.r].apply(null,message.p).then(function(res){
+					spark.write({i:message.i, m:res,s:1});
+				}).catch(function(err) {
+					err = serializeError(err);
+					spark.write({i: message.i, m: err, s: 0});
+				});
+			}
 		});
 	}, serverOpt);
 }
@@ -44,35 +43,35 @@ PrimusTransport.prototype._connect = function(url,port,id){
 	var address = url+':'+port+'#'+id;
 	if(!this._connections[address]){
 		this._connections[address] = new this._Studio.promise(function(resolve,reject){
-            var scheme = _self.isSecure ? 'wss://' : 'ws://';
-            var client = new _self._server.Socket(scheme+url+":"+port+"/?svc="+encodeURIComponent(id),_self._clientOpt);
-            client.on('open', function(){resolve(client);});
-            client.on('timeout',reject);
-            client.on('end',function(){_self.emit('end',{url:url,port:port,id:id});});
-            client.on('close',function(){_self.emit('close',{url:url,port:port,id:id});});
-            client.on('destroy',function(){_self.emit('end',{url:url,port:port,id:id});});
-            client.on('disconnect',function(){_self.emit('end',{url:url,port:port,id:id});});
-            client.on('reconnected',function(){_self.emit('reconnected',{url:url,port:port,id:id});});
-            client.on('close',function(){
-            	// Reject all current promises, at this point we dont know if its going to be finished
-	            Object.keys(client._promises).forEach(function(k){
-	                client._promises[k].reject(new Error('CLOSED'));
-	            });
+			var scheme = _self._clientOpt.isSecure ? 'wss://' : 'ws://';
+			var client = new _self._server.Socket(scheme+url+":"+port+"/?svc="+encodeURIComponent(id),_self._clientOpt);
+			client.on('open', function(){resolve(client);});
+			client.on('timeout',reject);
+			client.on('end',function(){_self.emit('end',{url:url,port:port,id:id});});
+			client.on('close',function(){_self.emit('close',{url:url,port:port,id:id});});
+			client.on('destroy',function(){_self.emit('end',{url:url,port:port,id:id});});
+			client.on('disconnect',function(){_self.emit('end',{url:url,port:port,id:id});});
+			client.on('reconnected',function(){_self.emit('reconnected',{url:url,port:port,id:id});});
+			client.on('close',function(){
+				// Reject all current promises, at this point we dont know if its going to be finished
+				Object.keys(client._promises).forEach(function(k){
+						client._promises[k].reject(new Error('CLOSED'));
+				});
 			});
 			client.on('end',function(){
 				// We decided to stop trying to reconnect, free variable so we can connect again if it restarrt
 				_self._connections[address] = null;
 			});
-            client.on('data',function(msg){
-            	var funk;
-            	if(msg && msg.i && client._promises[msg.i]){
-                    funk = msg.s ? client._promises[msg.i].resolve : client._promises[msg.i].reject;
-                    delete client._promises[msg.i];
-                    funk(msg.m);
-                }
-            });
-            client._promises={};
-        });
+			client.on('data',function(msg){
+				var funk;
+				if(msg && msg.i && client._promises[msg.i]){
+							funk = msg.s ? client._promises[msg.i].resolve : client._promises[msg.i].reject;
+							delete client._promises[msg.i];
+							funk(msg.m);
+					}
+			});
+			client._promises={};
+		});
 	}
 };
 PrimusTransport.prototype.send = function(url,port,id,params,receiver){
